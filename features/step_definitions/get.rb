dbb7bb6fc7d71cd1ca8e1603f0dc2bbfb6d073d6
Lucas Martins <lucascamargogoncalvesmartins@gmail.com>
2026-03-11 15:42:13 -0300

    teste funcionando

--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -1,12 +1,13 @@
 Dado('que o usuario consulte informacoes de um funcionario') do
-  @teste = HTTParty.get('http://dummy.restapiexample.com/api/v1/employees')
-  puts @teste
+  @get_url = 'http://dummy.restapiexample.com/api/v1/employees'
 end
 
 Quando('ele realizar a pesquisa') do
-  
+  @list_employees = HTTParty.get(@get_url)
+  puts "A resposta da API foi: #{@list_employees.inspect}"
 end
 
 Entao('uma lista de funcionarios deve retornar') do
-  
+  expect(@list_employees.code).to eql 200
+  expect(@list_employees.message).to eql 'OK'
 end
